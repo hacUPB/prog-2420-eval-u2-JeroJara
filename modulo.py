2c3f0cb4c7b1a7a7bd53aa0e090b8d8b9dffcdb8
Jerónimo JaramilloRivvvv <jeronimo.jaramillor@upb.edu.co>
2024-09-13 22:39:29 -0500

    Delete modulo.py

--- modulo.py
+++
@@ -1,7 +0,0 @@
-def registro(invocativo,nombre,apellido):
-    if invocativo.lower() == "señor" or invocativo.lower() == "señora":
-        return print("¡Gracias por elegirnos! Bienvenido a la mejor Aerolínea del barrio, ",invocativo, nombre, apellido)
-    else:
-        print("Género no válido")
-
-
